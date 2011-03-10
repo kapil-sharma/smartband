ea5e601b056414751d47b35d94af23434bcc6cf1
oriolventura@hotmail.com <oriolventura@hotmail.com@baa33456-502e-9cd8-5dcc-6afcc7a6bbb3>
2011-03-10 17:10:12 +0000

    Primea versió Hello World

---
+++ trunk/SmartBand/src/com/ub/pis04/smartband/SmartBand.java
@@ -0,0 +1,13 @@
+package com.ub.pis04.smartband;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmartBand extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
