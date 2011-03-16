6b517b4b918c63b523c5fe7365828f872df51d55
oriolventura@hotmail.com <oriolventura@hotmail.com@baa33456-502e-9cd8-5dcc-6afcc7a6bbb3>
2011-03-16 18:24:10 +0000

    proves

--- trunk/SmartBand/src/com/ub/pis04/smartband/widgets/TabSheet.java
+++ trunk/SmartBand/src/com/ub/pis04/smartband/widgets/TabSheet.java
@@ -1,7 +1,5 @@
 package com.ub.pis04.smartband.widgets;
 
-import java.util.ArrayList;
-
 import com.ub.pis04.smartband.R;
 
 import android.content.Context;
@@ -13,22 +11,29 @@ import android.view.View;
 
 public class TabSheet extends View {
 
-    int width, heght, numCols, colIni, colFi, posX, posY;
-	ArrayList cols = new ArrayList();
-    
+    Bitmap bmp = null;
+	int width, heght, numCols;
+	int colIni, colFi, posX;
+	int posY, colWidth, colHeight;
+    int posAnt = 0;
+	int direccio = 0;
+	
 	public TabSheet(Context context) {
 		super(context);
 		
+		bmp = BitmapFactory.decodeResource(getResources(), R.drawable.col);
+		colWidth = bmp.getWidth();
+		colHeight = bmp.getHeight();
 		width = getWidth();
 		heght = getHeight();
-		numCols = (width / TabCol.width) + 1;
+		numCols = (width / colWidth) + 1;
 		colIni = 0;
 		colFi = numCols;
 		posX = 0;
 		posY = 100;
 		
 		for(int i=0; i< numCols; i++){
-			cols.add(new TabCol(context));
+		
 		}
 	}
 
@@ -36,15 +41,21 @@ public class TabSheet extends View {
 	protected void onDraw(Canvas canvas) {
 		
 		for(int i = colIni; i< colFi; i++){
-			TabCol tb = (TabCol)cols.get(i);
-			tb.setX(posX);
-			tb.setY(PosY);
-			tb.draw(canvas);
+			
 		}
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		return super.onTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_MOVE) {
+			if(posAnt != 0) posAnt = (int)event.getX();
+			direccio = posAnt - (int)event.getX();
+			if(direccio > 0){ //Movem dreta
+				
+			} else{  //Movem esquerra
+				
+			}
+		}
+		return true;
 	}
 }
